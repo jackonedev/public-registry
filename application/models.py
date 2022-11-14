d69fd3d306cd80ba9307a9c145191143c340130c
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-14 10:09:44 -0300

    models-app: Person, Address created

--- application/models.py
+++ application/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    id = models.IntegerField(max_length=10, primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField(max_length=3)
+    #picture = models.ImageField(upload_to='media/person', blank=True)
+
+    def __str__(self) -> str:
+        s = "{} {}, dni: {}"
+        return s.format(self.first_name, self.last_name, self.id)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=30)
+    number = models.IntegerField(max_length=10)
+    city = models.CharField(max_length=30)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        s = "{}, {} {}, {}"
+        return s.format(self.person, self.street, self.number, self.city)
