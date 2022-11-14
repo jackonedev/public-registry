61b85c243478fa9c9b8d9281bd7cb3a21d494ec3
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-14 14:39:53 -0300

    third model migration

--- application/models.py
+++ application/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Person(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField('DNI', max_length=30, primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
+    age = models.PositiveIntegerField()
     #picture = models.ImageField(upload_to='media/person', blank=True)
 
     def __str__(self) -> str:
