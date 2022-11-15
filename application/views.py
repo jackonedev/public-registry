f17f8a1ba83fb2be4017ff9729ed3f11aa2fda59
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-15 14:29:31 -0300

    views-app: add 'person_list' GET endpoint

--- application/views.py
+++ application/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from application import serializers
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from .models import Person, Address
+from .serializers import PersonSerializer, AddressSerializer
+
+
+@api_view(['GET'])
+def person_list(request):
+    persons = get_object_or_404(Person.objects.all())
+    # persons = Person.objects.all()
+    serializer = PersonSerializer(persons, many=True)
+    return Response(serializer.data)
