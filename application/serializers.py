8a7110209a860c543fdb3aa0684ec3e9cc5e6f7c
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-15 14:21:17 -0300

    serializers-app: add file

---
+++ application/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Person, Address
+
+
+class AddressSerializer(ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+class PersonSerializer(ModelSerializer):
+    class Meta:
+        model = Person
+        fields = '__all__'
