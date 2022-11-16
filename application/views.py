bca8186dcf3a4c6c99b0e17ba3059d8ec80272d9
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-16 15:36:06 -0300

    views-app: add status code to Response, and managing assertions

--- application/views.py
+++ application/views.py
@@ -1,11 +1,7 @@
-from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from application import serializers
-
-
-
-from django.shortcuts import get_object_or_404
 
 from .models import Person, Address
 from .serializers import PersonSerializer, AddressSerializer
@@ -17,11 +13,6 @@ def getAllPerson(request):
     serializer = PersonSerializer(persons, many=True)
     return Response(serializer.data)
 
-# @api_view(['GET'])
-# def getPerson(request,first_name):#TODO='', lname='', age=''):
-#     persons = Person.objects.filter(first_name=first_name)
-#     serializer = PersonSerializer(persons, many=True)
-#     return Response(serializer.data)
 
 @api_view(['GET'])
 def getPerson(request):
@@ -51,39 +42,36 @@ def getPerson(request):
         persons = Person.objects.filter(age=age)
     
     else:
-        return Response(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     serializer = PersonSerializer(persons, many=True)
     return Response(serializer.data)
 
 
-
 @api_view(['GET'])
 def getPersonById(request):
     id = request.query_params.get('id', None)
+    
+    if not id:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     if not id.startswith('*') and not id.endswith('*'):
         persons = Person.objects.filter(id=id)
-        serializer = PersonSerializer(persons, many=True)
-        return Response(serializer.data)
 
     elif id.startswith('*') and id.endswith('*'):
         id = id.replace('*', '')
         persons = Person.objects.filter(id__icontains=id)
-        serializer = PersonSerializer(persons, many=True)
-        return Response(serializer.data)
 
     elif id.startswith('*'):
         id = id.replace('*', '')
         persons = Person.objects.filter(id__iendswith=id)
-        serializer = PersonSerializer(persons, many=True)
-        return Response(serializer.data)
 
     elif id.endswith('*'):
         id = id.replace('*', '')
         persons = Person.objects.filter(id__istartswith=id)
-        serializer = PersonSerializer(persons, many=True)
-        return Response(serializer.data)
 
-    else:#
-        return Response(status=404)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PersonSerializer(persons, many=True)
+    return Response(serializer.data)
