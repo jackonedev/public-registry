d79bf8585b86af5c07af142d0271c1be30a800ba
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-17 12:37:14 -0300

    forms-app: add 'IdForm' for '/get/id/' endpoint

--- application/forms.py
+++ application/forms.py
@@ -4,3 +4,6 @@ class PersonForm(forms.Form):
     first_name = forms.CharField(max_length=50, required=False)
     last_name = forms.CharField(max_length=50, required=False)
     age = forms.IntegerField(required=False)
+
+class IdForm(forms.Form):
+    id = forms.CharField(required=True)
