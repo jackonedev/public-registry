60e04a383d0d1f56c1f0e65eb29bf3d7d8104acf
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-17 14:19:09 -0300

    forms-app: update file

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
+from django.forms import ModelForm
+
+from .models import Person, Address
+
 class PersonForm(forms.Form):
     first_name = forms.CharField(max_length=50, required=False)
     last_name = forms.CharField(max_length=50, required=False)
@@ -7,3 +11,14 @@ class PersonForm(forms.Form):
 
 class IdForm(forms.Form):
     id = forms.CharField(required=True)
+
+
+class PersonModelForm(ModelForm):
+    class Meta:
+        model = Person
+        fields = '__all__'
+
+class AddressModelForm(ModelForm):
+    class Meta:
+        model = Address
+        fields = '__all__'
