5c2537bebc2b0da0d632da12a8c906cab01c8d29
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-17 11:32:32 -0300

    urls-pjt: add path to home

--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from application import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('api/', include('application.urls')),
 ]
 
