141533fe2c572b13563efcd4e7fc0b8ca8f13f6e
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-22 15:20:22 -0300

    updated ModelForms

--- application/forms.py
+++ application/forms.py
@@ -5,20 +5,46 @@ from django.forms import ModelForm
 from .models import Person, Address
 
 class PersonForm(forms.Form):
-    first_name = forms.CharField(max_length=50, required=False)
-    last_name = forms.CharField(max_length=50, required=False)
-    age = forms.IntegerField(required=False)
+    first_name = forms.CharField(max_length=50, required=False, label='Name' ,widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Name'}))
+    last_name = forms.CharField(max_length=50, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    age = forms.IntegerField(required=False, widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
 class IdForm(forms.Form):
-    id = forms.CharField(required=True)
+    id = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
 
 class PersonModelForm(ModelForm):
     class Meta:
         model = Person
         fields = '__all__'
+        labels = {
+            'id': 'DNI',
+            'first_name': 'Name',
+            'last_name': 'Last Name',
+            'age': 'Age',
+            'picture': 'Select picture',
+        }
+        widgets = {
+            'id': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control'}),
+            'picture': forms.FileInput(attrs={'class': 'form-control'}),
+        }
 
 class AddressModelForm(ModelForm):
     class Meta:
         model = Address
         fields = '__all__'
+        labels = {
+            'person': 'DNI',
+            'street': 'Street',
+            'number': 'Number',
+            'city': 'City'
+        }
+        widgets = {
+            'person': forms.TextInput(attrs={'class': 'form-control'}),
+            'street': forms.TextInput(attrs={'class': 'form-control'}),
+            'number': forms.NumberInput(attrs={'class': 'form-control'}),
+            'city': forms.TextInput(attrs={'class': 'form-control'})
+        }
