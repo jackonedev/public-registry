cc5d46374789622936ba97588b8258a8e9f0264d
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-22 16:30:11 -0300

    update view name

--- application/urls.py
+++ application/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'application'
 
 urlpatterns = [
-    path('', views.app_home, name='app_home'),
+    path('', views.app_home, name='home'),
     path('get/', views.get, name='get'),
     path('post/', views.post, name='post'),
     path('person/download/', views.download, name='download'),
