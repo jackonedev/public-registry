21c3a758d3ff0a5f5696c01a747ed43f1cad4699
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-23 10:44:18 -0300

    refactoring api rest views

--- application/api/views.py
+++ application/api/views.py
@@ -21,28 +21,20 @@ def getPerson(request):
 
     if first_name and last_name and age:
         persons = Person.objects.filter(first_name=first_name, last_name=last_name, age=age)
-
     elif first_name and last_name:
         persons = Person.objects.filter(first_name=first_name, last_name=last_name)
-    
     elif first_name and age:
         persons = Person.objects.filter(first_name=first_name, age=age)
-
     elif last_name and age:
         persons = Person.objects.filter(last_name=last_name, age=age)
-    
     elif first_name:
         persons = Person.objects.filter(first_name=first_name)
-
     elif last_name:
         persons = Person.objects.filter(last_name=last_name)
-
     elif age:
         persons = Person.objects.filter(age=age)
-    
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
     if len(persons) == 0:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -56,22 +48,17 @@ def getPersonById(request):
     
     if not id.startswith('*') and not id.endswith('*'):
         persons = Person.objects.filter(id=id)
-
     elif id.startswith('*') and id.endswith('*'):
         id = id.replace('*', '')
         persons = Person.objects.filter(id__icontains=id)
-
     elif id.startswith('*'):
         id = id.replace('*', '')
         persons = Person.objects.filter(id__iendswith=id)
-
     elif id.endswith('*'):
         id = id.replace('*', '')
         persons = Person.objects.filter(id__istartswith=id)
-
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
     if len(persons) == 0:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
